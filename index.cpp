35b091f08d64f5902ce618eba955b183916a9529
DHRUVNJAIN <dj2429194@gmail.in>
2025-06-02 00:36:02 +0530

    friend feature

--- index.cpp
+++ index.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include<string>
 using namespace std;
+class Friend {
+    public:
+string name;
+string age;
+float udhar;
+void changeage (string newage ){
+    age =newage; 
+}
+};
 int main(){
-    cout<<"hello world";
+Friend F1;
+F1.name = "chtn ";
+cout<<F1.name << endl;
+   
 return 0;
 };
